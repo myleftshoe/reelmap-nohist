560d2580ea696f8234f4f11686227f4b9f14d17b
Paul Sammut <paulesammut@gmail.com>
2019-03-25 15:19:00 +1100

    vroom removed capacity and amt

--- src/map/services/vroom2.js
+++ src/map/services/vroom2.js
@@ -1,3 +1,16 @@
+const depot = [145.005252, -37.688797];
+
+// const vehicleProps = {
+//     'CHA': { timeWindow: [36000, 61200], capacity: 100 },
+//     'DRK': { timeWindow: [36000, 61200], capacity: 100 },
+//     'SAM1': { timeWindow: [36000, 61200], capacity: 100 },
+// }
+
+
+// const shiftDurations = [1, .9, .6]
+
+// const totalDuration = shiftDurations.reduce((a, b) => a + b, 0)
+
 async function doFetch(payload) {
     const response = await fetch(`http://localhost:3000/`, {
         method: 'POST',
@@ -17,26 +30,33 @@ export default async function vroom(items = [], drivers = []) {
 
     const itemsMap = new Map([...items.map(item => [item.OrderId, item])]);
     // const items = [...itemsMap.values()];
-    const depot = [145.005252, -37.688797];
+    // const depot = [145.005252, -37.688797];
     // const drivers = ['CHA'];
 
     const jobs = items.map(item => ({
         id: parseInt(item.OrderId, 10),
         location: [item.GeocodedAddress.longitude, item.GeocodedAddress.latitude],
-        amount: [1],
+        // amount: [1],
         skills: [item.Driver ? drivers.indexOf(item.Driver) + 1 : 0]
     }));
 
-    const capacity = [Math.ceil(1.2 * items.length / drivers.length)]
+    // const capacity = [Math.ceil(1.2 * items.length / drivers.length)]
 
-    const vehicles = drivers.map((_, index) => ({
-        "id": index,
-        "start": depot,
-        "end": depot,
-        "time_window": [36000, 61200],
-        capacity,
-        skills: [0, index + 1]
-    }));
+    // const totalJobs = items.length;
+    // const _totalDuration = (drivers.length === 1) ? 1 : totalDuration;
+    // const capacityFactor = totalJobs / _totalDuration;
+    // console.log(capacityFactor)
+    const vehicles = drivers.map((driver, index) => {
+        // console.log(driver, shiftDurations[index], Math.ceil(capacityFactor * shiftDurations[index]))
+        return {
+            "id": index,
+            "start": depot,
+            "end": depot,
+            "time_window": [36000, 61200],
+            // capacity: [Math.ceil(capacityFactor * shiftDurations[index]) + 2],
+            skills: [0, index + 1]
+        }
+    });
 
     const options = { "g": true }; //returns route geometry
 
