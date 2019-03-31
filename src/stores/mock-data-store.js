950fd75d74a0c5922a26b6ce733a5c7ff776634b
Paul Sammut <paulesammut@gmail.com>
2019-03-31 16:20:31 +1100

    minor

--- src/stores/mock-data-store.js
+++ src/stores/mock-data-store.js
@@ -5,7 +5,10 @@ export default function dataStore() {
 
     const persist = false;
     const [state, setState] = useState(new Map(data.map(data => [data.OrderId, data])));
-    const update = () => setState(new Map(state));
+    function update() {
+        console.log('Updating mock-data-store');
+        setState(new Map(state));
+    }
     const storeFromArray = (items) => setState(new Map(items.map(data => [data.OrderId, data])));
     return [state, update, persist, storeFromArray];
 
