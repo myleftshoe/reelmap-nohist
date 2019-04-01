2187476143dc691baa5f74188ee81ed110e4d77e
Paul Sammut <paulesammut@gmail.com>
2019-04-01 17:15:31 +1100

    Adjusted theme-switcher in accordance with custom-control-bar refactoring

--- src/map/theme-switcher.js
+++ src/map/theme-switcher.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { CustomControl } from '@googlemap-react/core'
 import styles from './styles/styles';
 import useLoop from '../hooks/useLoop';
 import CustomControlBar from './custom-control-bar';
 
 export default function ThemeSwitcher({ map }) {
     const nextKey = useLoop([...Object.keys(styles), null], localStorage.getItem('mapTheme'));
-    const switchTheme = () => {
+    function switchTheme() {
         const themeName = nextKey();
         map.setOptions({ styles: styles[themeName] })
         localStorage.setItem('mapTheme', themeName);
     }
-    return <CustomControl bindingPosition='TOP_LEFT'>
-        <CustomControlBar>
-            <CustomControlBar.IconButton onClick={switchTheme}>invert_colors</CustomControlBar.IconButton>
-            {/* <CustomControlBar.Divider/> */}
-            {/* <CustomControlBar.TextButton onClick={switchTheme}>Satellite</CustomControlBar.TextButton> */}
-        </CustomControlBar>
-    </CustomControl>
+    return <CustomControlBar small>
+        <CustomControlBar.IconButton onClick={switchTheme}>invert_colors</CustomControlBar.IconButton>
+        {/* <CustomControlBar.Divider/> */}
+        {/* <CustomControlBar.TextButton onClick={switchTheme}>Satellite</CustomControlBar.TextButton> */}
+    </CustomControlBar>
 }
