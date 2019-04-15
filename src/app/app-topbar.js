092c5d72748fe6d0a4ee6dcd1712946503639f6b
Paul Sammut <paulesammut@gmail.com>
2019-04-15 11:59:06 +1000

    app-topbar

---
+++ src/app/app-topbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Resizable from '../common/resizable'
+import { resizableProps } from '../common/constants'
+import { useToast } from '../toasts'
+import Sidebars from '../sidebar/sidebars';
+import MapContainer from '../map/map-container';
+import TextButton from '../common/text-button';
+
+const openInNew = id => window.open(`http://localhost:3006/${id}`)
+
+function App({ state, dispatch }) {
+
+  console.log('rendering', state.activeItems);
+  useToast(state.toast);
+
+  return <div style={{ display: 'flex', flexDirection: 'column' }}>
+    <div style={{ flex: '0 0 40px', width: '100vw', backgroundColor: '#2c2c2f', display:'flex', flexDiretion:'row', justifyContent: 'space-evenly' }}>
+      <TextButton color='white'>Assign</TextButton>
+      <div style={{flex: '0 0 1px', backgroundColor:'#fff3' }}/>
+      <TextButton color='white'>Plan</TextButton>
+    </div>
+    <div>
+      <Resizable split='vertical' {...resizableProps}>
+        <Sidebars state={state} dispatch={dispatch} />
+        <MapContainer state={state} dispatch={dispatch} />
+      </Resizable >
+    </div>
+  </div>
+}
+
+export default App;
