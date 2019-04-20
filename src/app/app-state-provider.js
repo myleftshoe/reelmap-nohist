ab636564b41fc018a499e8e7533e24f54865e149
Paul Sammut <paulesammut@gmail.com>
2019-04-20 19:20:21 +1000

    minor

--- src/app/app-state-provider.js
+++ src/app/app-state-provider.js
@@ -197,7 +197,7 @@ export default function StateProvider(props) {
     }
 
     function MaximizeEnd(id) {
-        setMapEditMode({ on: false, id: null, tool: null })
+        // setMapEditMode({ on: false, id: null, tool: null })
         setSelectedDrivers(id ? [id] : [])
         setMaxPaneId(id)
     }
